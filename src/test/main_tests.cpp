cba328ebb1d4675d3e7c2e912938c65cfd5cfde4
DeckerSU <deckersu@protonmail.com>
2022-03-23 23:12:57 +0300

    fix subsidy_limit_test test

--- src/test/main_tests.cpp
+++ src/test/main_tests.cpp
@@ -51,11 +51,11 @@ BOOST_AUTO_TEST_CASE(subsidy_limit_test)
     CAmount nSum = 0;
     for (int nHeight = 0; nHeight < 14000000; nHeight += 1000) {
         CAmount nSubsidy = GetBlockSubsidy(nHeight, chainParams->GetConsensus());
-        BOOST_CHECK(nSubsidy <= 50 * COIN);
+        BOOST_CHECK(nSubsidy <= 0 * COIN);
         nSum += nSubsidy * 1000;
         BOOST_CHECK(MoneyRange(nSum));
     }
-    BOOST_CHECK_EQUAL(nSum, 2099999997690000ULL);
+    BOOST_CHECK_EQUAL(nSum, 0ULL);
 }
 
 bool ReturnFalse() { return false; }
