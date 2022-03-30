1d07da6785817d1418c01da1011b756db7df2625
dimxy <dimxy@komodoplatform.com>
2022-03-30 22:31:54 +0500

    check strlcpy/cat on darwin

--- src/cryptoconditions/src/strl.h
+++ src/cryptoconditions/src/strl.h
@@ -1,17 +1,28 @@
-#include <sys/types.h>
-#include <string.h>
-
+// strlxxx safe function impl for linux
 #ifndef STRL_H
 #define STRL_H
+
+#ifdef __APPLE__  && __DARWIN_C_LEVEL >= __DARWIN_C_FULL
+#undef HAVE_STRLCPY
+#undef HAVE_STRLCAT
+#define HAVE_STRLCPY 1
+#define HAVE_STRLCAT 1
+#endif
+
+#include <sys/types.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
+
 #ifndef HAVE_STRLCPY
 size_t
 strlcpy(char *dst, const char *src, size_t dsize);
 #endif
+
 
 #ifndef HAVE_STRLCAT
 size_t
